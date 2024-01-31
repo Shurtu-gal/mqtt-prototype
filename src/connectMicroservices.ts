a9215d4857dff41cdd288a78c9d22a01cf647956
Ashish Padhy <100484401+Shurtu-gal@users.noreply.github.com>
2024-01-31 22:08:06 +0530

    feat: setup initial mqtt

--- src/connectMicroservices.ts
+++ src/connectMicroservices.ts
@@ -1,6 +1,9 @@
 import { INestApplication } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
+import { MicroserviceOptions } from "@nestjs/microservices";
+import { generateClientOptions } from "./mqtt/generateClientOptions";
 
 export async function connectMicroservices(app: INestApplication) {
   const configService = app.get(ConfigService);
+  app.connectMicroservice<MicroserviceOptions>(generateClientOptions(configService));
 }
